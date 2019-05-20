01a8927fb8682f76e0705f21b3d5e460524d989b
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-20 16:38:52 -0400

    added background

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+let div1 = document.getElementById("div1")
+window.addEventListener("scroll", function(){
+  console.log(window.scrollY)
+  if(window.scrollY > 0) {
+  div1.classList.remove("hidden")
+  div1.classList.add("show")
+}
+})
