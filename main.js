5d3020a8bb6f23a9e1b09ef1fd60478d2f08b71d
Ashley Almonte <ashleyalmonte14@gmail.com>
2019-05-30 11:15:29 -0400

    updated

--- main.js
+++ main.js
@@ -1,60 +1,10 @@
-requestAPI = () => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let data = JSON.parse(this.responseText)
-            user = {
-                name: data['name'],
-                speacial_attack: data["stats"][2]["base_stat"],
-                hp: data["stats"][5]["base_stat"],
-                defense:data["stats"][1]["base_stat"],
-                ability:data["abilities"][1]["ability"]["name"]
-
-            }
-        }
-    };
-    xhttp.open("GET", " https://fizal.me/pokeapi/api/v2/name/pikachu.json", true);
-}
-requestAPI = () => {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            let data = JSON.parse(this.responseText)
-            user = {
-                name: data['name'],
-                speacial_attack: data["stats"][4]["base_stat"],
-                hp: data["stats"][5]["base_stat"],
-                defense:data["stats"][1]["base_stat"],
-                ability:data["abilities"][2]["ability"]["name"]
-              }
-              }
-            };
-xhttp.open("GET", " https://fizal.me/pokeapi/api/v2/name/jigglypuff.json", true)
-
-}
-
-
-
-requestAPI()
-,
-displayUser = () => {
-    let cont = document.createElement('div')
-    let h1 = document.createElement('h1')
-    let p1 = document.createElement('p')
-    let img = document.createElement('img')
-
-    h1.innerText = user['name']
-    p1.innerText = user['followers']
-    img.setAttribute('src', user['image'])
-    cont.appendChild(h1)
-    cont.appendChild(p1)
-    cont.appendChild(img)
-    document.body.appendChild(cont)
-}
-
 window.onscroll = function() {myFunction()};
+
+
 var navbar = document.getElementById("navbar");
+
 var sticky = navbar.offsetTop;
+
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
@@ -63,26 +13,51 @@ function myFunction() {
   }
 }
 
+requestAPI = () => {
+  const Pokemon = prompt("Enter a pokemon")
+    var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let data = JSON.parse(this.responseText)
+            user = {
+                name: data['name'],
+                attack: data["stats"][4]["base_stat"],
+                hp: data["stats"][5]["base_stat"],
+                defense:data["stats"][3]["base_stat"],
+                ability:data["abilities"][0]["ability"]["name"]
 
-function openCity(cityName, elmnt, color) {
+            }
+            displayUser()
 
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
-
-  document.getElementById(cityName).style.display = "block";
-
-  elmnt.style.backgroundColor = color;
+        }
+    };
+        xhttp.open("GET", `https://fizal.me/pokeapi/api/v2/name/${Pokemon}.json`, true);
+    xhttp.send();
 }
+ let cont = document.createElement('div')
+displayUser = () => {
+    let cont = document.createElement('div')
+    let h1 = document.createElement('h1')
+    let p1 = document.createElement('p')
+    let p2 = document.createElement('p2')
+    let p3 = document.createElement('p3')
+    let p4 = document.createElement('p4')
+    let img = document.createElement('img')
+
+  h1.innerText = user['name']
+  p1.innerText = user['defense']
+  p2.innerText = user['attack']
+  p3.innerText = user['hp']
+  p4.innerText = user['ability']
+  img.setAttribute('src', user['sprite'])
+   cont.appendChild(h1)
+    cont.appendChild(p1)
+    cont.appendChild(p2)
+    cont.appendChild(p3)
+    cont.appendChild(p4)
+    cont.appendChild(img)
+    document.body.appendChild(cont)
 
 
-document.getElementById("defaultOpen").click();
 }
+requestAPI()
